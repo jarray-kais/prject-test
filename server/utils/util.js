80a9f9d2c8ccd109c1b360f08ced3a66944343da
jarray-kais <jarraykais1@gmail.com>
2025-11-06 12:36:39 +0100

    Add role field to User model and implement admin middleware
    Signed-off-by: jarray-kais <jarraykais1@gmail.com>

--- server/utils/util.js
+++ server/utils/util.js
@@ -26,3 +26,10 @@ export const authMiddleware = (req, res, next) => {
         next();
     });
 };
+
+export const isAdmin = (req, res, next) => {
+    if (req.user.role !== "admin") {
+        return res.status(403).json({ message: "Unauthorized: Admin only can access this route" });
+    }
+    next();
+};
